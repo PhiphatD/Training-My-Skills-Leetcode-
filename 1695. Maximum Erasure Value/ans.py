0f89103fb9e30facb5634d42e2554e100b45dd30
PhipahatD <220616026+PhiphatD@users.noreply.github.com>
2025-07-22 23:46:48 +0700

    253 ms 21 mb

---
+++ 1695. Maximum Erasure Value/ans.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maximumUniqueSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        seen = set()
+        left = 0
+        current = 0
+        maxsum = 0
+        for right in range(len(nums)):
+            while nums[right]  in seen:
+                seen.remove(nums[left])
+                current -= nums[left]
+                left += 1
+            seen.add(nums[right])
+            current += nums[right]
+            maxsum = max(current,maxsum)
+        return maxsum
+
+        
+nums = [4,2,4,5,6]
+solution = Solution()
+result = solution.maximumUniqueSubarray(nums)
+print(result)  # Output: 0 (or the expected output based on the implementation)
