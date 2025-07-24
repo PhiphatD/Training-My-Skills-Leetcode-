235a1ef4b45248e74ceb01b1a8c60030ec4d3131
PhipahatD <220616026+PhiphatD@users.noreply.github.com>
2025-07-25 00:13:24 +0700

    0ms 12.34mb

---
+++ 66. Plus One/Ans.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        
+        num_int = ""
+        for i in digits :
+            num_int += str(i) 
+        
+        plusone = int(num_int) + 1
+        new_digit  = []
+        for i in str(plusone) :
+            new_digit.append(int(i))
+        
+        return new_digit
+    
+    
+    
+    
+    
+digits = [0]  
+solution = Solution()
+result = solution.plusOne(digits)
+print(result)
