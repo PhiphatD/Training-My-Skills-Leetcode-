42da201d74f8c85ca0b952f1341442c2a0244463
PhipahatD <220616026+PhiphatD@users.noreply.github.com>
2025-07-25 00:00:01 +0700

    2ms Beats 12.38%  12 mb

--- 58. Length of Last Word/Ans.py
+++ 58. Length of Last Word/Ans.py
@@ -4,23 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        qty = 0
-        max_length = 0
-        i = len(s)-1 
-        while i >= 0 and s[i] != " ":
-            qty += 1
-            i -= 1
-        while i >= 0 and s[i] == " ":
-            if qty > max_length :
-                max_length = qty 
-                qty = 0
-            i -= 1
-        return max_length
-
+        
+        s = s.strip()
+        word = s.split()
+        return len(word[-1])
     
     
-    
-s = "   fly me   to   the moon  "
+s = "Today is a nice day"
 solution = Solution()
 reusult = solution.lengthOfLastWord(s)
 print(reusult)
