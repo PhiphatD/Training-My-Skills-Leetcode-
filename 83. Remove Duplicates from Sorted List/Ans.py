0927f5cf05b5a8907e736ec6ffa7549039125298
PhipahatD <220616026+PhiphatD@users.noreply.github.com>
2025-07-25 03:02:11 +0700

    4 ms 12.54mb

---
+++ 83. Remove Duplicates from Sorted List/Ans.py
@@ -0,0 +1,43 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+def create_linked_list(lst):
+    dummy = ListNode(0)
+    current = dummy
+    for num in lst:
+        current.next = ListNode(num)
+        current = current.next
+    return dummy.next
+
+
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(head.val)
+        current = dummy
+        
+        while head :
+            if current.val == head.val :
+                
+                head = head.next
+            elif current.val != head.val :
+                current.nex = ListNode(head.val)
+                head = head.next
+
+        return dummy
+
+
+
+
+
+head = [1,1,2] 
+solution = Solution()
+head = create_linked_list(head)
+result = solution.deleteDuplicates(head)
+
+
